3697a25704403f9784a2a8de14421fc1ce998d23
ezequiel coronel <roqueezequielcoronel93@gmail.com>
2025-06-07 12:50:30 -0300

    cambie nombre al sub menu libro

--- elLibrito_Feliz/menuLibro.cpp
+++ elLibrito_Feliz/menuLibro.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
 #include "menuLibro.h"
 using namespace std ;
-void menuLibro::mostrarMenuLibro() {
-  int opcion ;
+void menuLibro::mostrarMenuLibro()
+{
+    int opcion ;
 
-    do {
-    cout << "---------------------------------------" << endl;
+    do
+    {
+        cout << "---------------------------------------" << endl;
 
-    cout << " SUB MENU SOCIO  " << endl;
-    cout << "1. Alta libro " << endl;
-    cout << "2.	Modificar libro "  << endl;
-    cout << "3.	Prestar libro "  << endl;
-    cout << "4.	Devolver libro "  << endl;
-    cout << "5.	Consultar stock "  << endl;
-    cout << "6.	Salir "  << endl;
+        cout << " SUB MENU  LIBRO   " << endl;
+        cout << "1. Alta libro " << endl;
+        cout << "2.	Modificar libro "  << endl;
+        cout << "3.	Prestar libro "  << endl;
+        cout << "4.	Devolver libro "  << endl;
+        cout << "5.	Consultar stock "  << endl;
+        cout << "6.	Salir "  << endl;
 
-    cout << "---------------------------------------" << endl;
+        cout << "---------------------------------------" << endl;
 
-    cout <<"ingrese una opcion " << endl;
-         cin >> opcion;
-    switch(opcion){
-       case 1 :
+        cout <<"ingrese una opcion " << endl;
+        cin >> opcion;
+        switch(opcion)
+        {
+        case 1 :
 
-        break;
+            break;
 
-       case 2 :
+        case 2 :
 
-       break;
-       case 3 :
+            break;
+        case 3 :
 
-        break;
-       case 4 :
+            break;
+        case 4 :
 
-        break;
-       case 5:
+            break;
+        case 5:
 
-        break;
+            break;
+        }
     }
-  }while(opcion!=6);
+    while(opcion!=6);
 
 
 }
